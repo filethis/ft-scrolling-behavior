4ff8a9987567ad55348c1574563c4da8c6bd78ef
Trent Brown <trent@filethis.com>
2020-03-25 16:53:23 -0700

    WIP

---
+++ demo/demo-fixture.js
@@ -0,0 +1,42 @@
+/* ft-scrolling-behavior element demo */
+/* Imports */
+/**
+
+A behavior that helps with scrolling of elements.
+
+@demo
+ */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import 'ft-element-demo/ft-element-demo.js';
+
+import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import '@polymer/polymer/polymer-legacy.js';
+import '../ft-scrolling-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
+Polymer
+({
+  _template: html`
+        <style include="iron-flex iron-flex-alignment iron-positioning"></style>
+
+        <style>
+            :host {
+                display: block;
+                overflow: hidden;
+            }
+        </style>
+
+        <ft-scrolling-behavior></ft-scrolling-behavior>
+`,
+
+  is: 'demo-fixture',
+
+  properties:
+  {
+  }
+});
